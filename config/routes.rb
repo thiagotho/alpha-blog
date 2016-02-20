7744fd55ac5ea15e6f266341ed3a88a9f29627c1
thiagotho <thiagootho@gmail.com>
2016-02-20 16:35:57 +0000

    added login route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
